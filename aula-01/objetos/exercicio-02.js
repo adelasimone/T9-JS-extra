af83146d84d08399ce42dade8b5808649897428a
Adela Simone <adelasimoneac@hotmail.com>
2020-06-08 15:18:51 -0300

    Exercicios objetos

--- aula-01/objetos/exercicio-02.js
+++ aula-01/objetos/exercicio-02.js
@@ -1,2 +1,11 @@
 /*Crie um objeto curso que receba as propriedades: nome, período, professora.
 Atribua valores as propriedades criadas e separadamente mostre as propriedades no console.log */
+
+
+const curso = {
+  nome: 'Adela',
+  periodo: 'Manha',
+  professora: 'Jess'
+}
+
+console.table([curso])
---
+++ aula-01/objetos/exercicio-03.js
@@ -0,0 +1,10 @@
+// Crie umobjeto curso que receba as prop.: nome, periodo, professora, nomeCurso
+// em seguida, escolha qual prop. vc queria acessar
+
+const curso = {
+  nome: 'Adela',
+  periodo: 'Manha',
+  professora: 'Jess'
+}
+
+console.log(curso.professora)
